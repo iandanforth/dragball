5e5cc715a44b87c320343b7f8243feaddbd0057f
Ian Danforth <iandanforth@gmail.com>
2018-03-23 20:40:55 -0700

    60fps with react-pixi-fiber ... and a bunny

--- app/src/features/home/CanvasTestPage.js
+++ app/src/features/home/CanvasTestPage.js
@@ -1,23 +1,53 @@
 import React, { Component } from 'react';
-import PaperContainer, { Circle, Layer } from '@psychobolt/react-paperjs';
+import { Sprite, Stage } from 'react-pixi-fiber';
+import * as PIXI from 'pixi.js';
+import bunny from '../../../bunny.png';
+
+
+function Bunny(props) {
+  return (
+    <Sprite texture={PIXI.Texture.fromImage(bunny)} {...props} />
+  );
+}
 
 export default class CanvasTestPage extends Component {
   static propTypes = {
 
   };
 
+  constructor() {
+    super();
+
+    this.state = {
+      bX: 0
+    };
+    this.updateAnim = this.updateAnim.bind(this);
+  }
+
+  componentDidMount() {
+    window.requestAnimationFrame(this.updateAnim);
+  }
+
+  translateString(x, y) {
+    return ` translate(${x},${y}) `;
+  }
+
+  updateAnim() {
+    this.setState({
+      bX: 400 + (Math.sin(Date.now() / 1000) * 100)
+    });
+    window.requestAnimationFrame(this.updateAnim);
+  }
+
   render() {
-    const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
+    const { bX } = this.state;
+    const bunnyTransform = this.translateString(bX, 0);
 
     return (
       <div className="home-canvas-test-page">
-        Component content: home/CanvasTestPage
-        <PaperContainer {...this.props}>
-          <Circle center={[80, 50]} radius={35} fillColor="red" />
-          <Layer>
-            <Shapes />
-          </Layer>
-        </PaperContainer>
+        <Stage width={800} height={600} options={{ backgroundColor: 0x10bb99 }}>
+          <Bunny x={bX} y={200} />
+        </Stage>
       </div>
     );
   }
